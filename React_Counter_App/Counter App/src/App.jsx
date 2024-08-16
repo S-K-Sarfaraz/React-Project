466d4b2bdc8d0b1df34f85579a50de101f571487
Sarfaraz <sarfaraz7856sk@gmail.com>
2024-08-16 13:22:01 +0530

    counter app completed with restriction of max 20 and min 0

--- React_Counter_App/Counter App/src/App.jsx
+++ React_Counter_App/Counter App/src/App.jsx
@@ -1,10 +1,34 @@
 import { useState } from 'react'
 
 function App(){
+
+  let [counter,setCounter] = useState(5)
+
+  function addValue(){
+    if(counter <20) {
+      counter= counter+1
+    }
+    setCounter(counter)
+  }
+
+  function subValue (){
+    if(counter>0){
+      counter = counter-1
+    }
+    setCounter(counter)
+  }
+
+  
   return (
     <>
-      <h1>Hello world</h1>
+      <h1>Counter Value</h1>
+      <h2>Current Value: {counter}</h2>
+      <button onClick={addValue}>Added Value</button>
       
+      <button onClick={subValue}>Remove Value</button>
+      <br />
+      <br />
+      <p>Review Value: {counter}</p>
     </>
   )
 }
